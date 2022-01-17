97f722794c7a6e8b05ca8e2fca1dfded6a19fbb1
Mario Salgado <mapsalgado92@gmail.com>
2022-01-17 17:03:03 +0000

    changed emailjs service

--- components/AddPickFrom.js
+++ components/AddPickFrom.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Row, Col, Image } from 'react-bootstrap'
-import { useState, useEffect } from 'react'
+import React from "react"
+import { Row, Col, Image } from "react-bootstrap"
+import { useState, useEffect } from "react"
 
 export const AddPickFrom = ({ setExtras, extras, index, picks }) => {
-  const [selected, setSelected] = useState(picks[0]);
+  const [selected, setSelected] = useState(picks[0])
 
   useEffect(() => {
     let newExtras = [...extras]
     newExtras[index].value = selected.price
     setExtras(newExtras)
-  }, [extras, index, selected, setExtras])
+  }, [index, selected, setExtras])
 
   const handleSelect = (pick) => {
     setSelected(pick)
@@ -25,20 +25,27 @@ export const AddPickFrom = ({ setExtras, extras, index, picks }) => {
         <h5>{extras[index].name}</h5>
         <h6>Select one of the following:</h6>
       </Col>
-      {picks && picks.map((pick) => {
-        let pickStyle = { backgroundColor: "white" }
-        if (selected.name === pick.name) {
-          pickStyle.backgroundColor = "#1f9bcf"
-
-        }
-        return (
-          <Col lg="3" md="4" xs="6" className="text-center my-2">
-            <p className="h5 mb-0">{pick.name}</p>
-            <Image onClick={() => handleSelect(pick)} src={pick.img} alt={pick.name} style={pickStyle} className="mb-1 p-1 border" fluid />
-            <p className="h5 text-info">{pick.price}€</p>
-          </Col>
-        )
-      })}
-    </Row >
+      {picks &&
+        picks.map((pick) => {
+          let pickStyle = { backgroundColor: "white" }
+          if (selected.name === pick.name) {
+            pickStyle.backgroundColor = "#1f9bcf"
+          }
+          return (
+            <Col lg="3" md="4" xs="6" className="text-center my-2">
+              <p className="h5 mb-0">{pick.name}</p>
+              <Image
+                onClick={() => handleSelect(pick)}
+                src={pick.img}
+                alt={pick.name}
+                style={pickStyle}
+                className="mb-1 p-1 border"
+                fluid
+              />
+              <p className="h5 text-info">{pick.price}€</p>
+            </Col>
+          )
+        })}
+    </Row>
   )
 }
