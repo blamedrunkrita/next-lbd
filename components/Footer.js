524fde85bbbeea4f86a2a5a6817055461b33180a
Mario Salgado <mapsalgado92@gmail.com>
2022-02-19 17:36:10 +0000

    added privacy policy and tos

--- components/Footer.js
+++ components/Footer.js
@@ -1,36 +1,78 @@
-import React from 'react'
-import { Container, Jumbotron, Image, Nav } from 'react-bootstrap'
-import Link from 'next/link'
+import React from "react"
+import { Container, Jumbotron, Image, Nav } from "react-bootstrap"
+import Link from "next/link"
 
 const Footer = () => {
   return (
     <footer id="footer">
-      <Jumbotron className="bg-primary mb-0 px-0 pt-4 pb-1">
+      <Jumbotron className="bg-primary mb-0 px-0 pt-2 pb-1">
         <Container className="text-center text-secondary">
-          <h2 className="mb-2" style={{ fontSize: "3em" }}>
-            <a className="mx-5" target="blank" href="https://www.facebook.com/lastbaddecision/"><i className="fa fa-facebook-square text-light" /></a>
-            <a className="mx-5" target="blank" href="https://www.instagram.com/lastbaddecision/?hl=pt"><i className="fa fa-instagram text-light" /></a>
-            <a className="mx-5" target="blank" href="https://api.whatsapp.com/send?phone=351914054039&text=Hi!%20I%27m%20interested%20in%20some%20of%20the%20Last%20Bad%20Decision%20activities."><i className="fa fa-whatsapp text-light" /></a>
-          </h2>
-          <Image src="/images/logo_white.png" alt="logo-white" height="50" />
-          <Nav className="justify-content-center h5 mt-2">
+          <div className="mb-2" style={{ fontSize: "2.5em" }}>
+            <a
+              className="mx-5"
+              target="blank"
+              href="https://www.facebook.com/lastbaddecision/"
+            >
+              <i className="fa fa-facebook-square text-light" />
+            </a>
+            <a
+              className="mx-5"
+              target="blank"
+              href="https://www.instagram.com/lastbaddecision/?hl=pt"
+            >
+              <i className="fa fa-instagram text-light" />
+            </a>
+            <a
+              className="mx-5"
+              target="blank"
+              href="https://api.whatsapp.com/send?phone=351914054039&text=Hi!%20I%27m%20interested%20in%20some%20of%20the%20Last%20Bad%20Decision%20activities."
+            >
+              <i className="fa fa-whatsapp text-light" />
+            </a>
+          </div>
+
+          <Nav className="justify-content-center h6 mt-2">
             <Nav.Item>
-              <Link href="/" passHref><Nav.Link className="text-light">Home</Nav.Link></Link>
+              <Link href="/" passHref>
+                <Nav.Link className="text-light">Home</Nav.Link>
+              </Link>
             </Nav.Item>
             <Nav.Item>
-              <Link href="/activities" passHref><Nav.Link className="text-light">Activities</Nav.Link></Link>
+              <Link href="/activities" passHref>
+                <Nav.Link className="text-light">Activities</Nav.Link>
+              </Link>
             </Nav.Item>
             <Nav.Item>
-              <Link href="/about" passHref><Nav.Link className="text-light">About Us</Nav.Link></Link>
+              <Link href="/about" passHref>
+                <Nav.Link className="text-light">About Us</Nav.Link>
+              </Link>
             </Nav.Item>
             <Nav.Item>
-              <Link href="/plan" passHref><Nav.Link className="text-light">Your Plan</Nav.Link></Link>
+              <Link href="/plan" passHref>
+                <Nav.Link className="text-light">Your Plan</Nav.Link>
+              </Link>
             </Nav.Item>
             <Nav.Item>
-              <Link href="/enquire/general" passHref><Nav.Link className="text-light">Enquiry</Nav.Link></Link>
+              <Link href="/enquire/general" passHref>
+                <Nav.Link className="text-light">Enquiry</Nav.Link>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link href="/terms" passHref>
+                <Nav.Link className="text-light">Terms & Conditions</Nav.Link>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link href="/privacy" passHref>
+                <Nav.Link className="text-light">Privacy Policy</Nav.Link>
+              </Link>
             </Nav.Item>
           </Nav>
-          <p id="signature" className="mt-3 text-light">Developed by | <span className="text-secondary">MarioJS</span></p>
+          <br></br>
+          <Image src="/images/logo_white.png" alt="logo-white" height="50" />
+          <p id="signature" className="mt-3 text-light">
+            Developed by | <span className="text-secondary">MarioJS</span>
+          </p>
         </Container>
       </Jumbotron>
     </footer>
---
+++ pages/terms.js
@@ -0,0 +1,41 @@
+import Head from "next/head"
+
+import { Container, Fade } from "react-bootstrap"
+
+const iframeStyles = {
+  width: "100%",
+  border: "none",
+  height: "52vh",
+}
+
+export default function Home(props) {
+  return (
+    <>
+      <Head>
+        <title>LBD | Privacy Policy</title>
+        <meta
+          name="description"
+          content="Craziest Tailored Stag, Hen and Group Weekends in Lisbon"
+        />
+      </Head>
+
+      <main>
+        <Fade appear={true} in={true}>
+          <div id="terms-page">
+            <section id="terms&conditions" className="text-center mt-4">
+              <Container>
+                <h1 className="title-text h3 border-bottom mb-0">
+                  Terms and Conditions
+                </h1>
+                <iframe
+                  src={"/terms&conditions.html"}
+                  style={iframeStyles}
+                ></iframe>
+              </Container>
+            </section>
+          </div>
+        </Fade>
+      </main>
+    </>
+  )
+}
